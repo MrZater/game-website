8a7760a397f97ada06366c33a2772f61b1ba8062
zt <zhoutao@ydmob.com>
2023-07-13 15:37:44 +0800

    style: 格式: 🎨 lint

--- commitlint.config.cjs
+++ commitlint.config.cjs
@@ -9,7 +9,7 @@ module.exports = {
     "body-leading-blank": [2, "always"],
     "footer-leading-blank": [1, "always"],
     "header-max-length": [2, "always", 108],
-    "subject-empty": [2, "never"],
+    "subject-empty": [0],
     "type-empty": [2, "never"],
     "subject-case": [0],
     "type-enum": [
